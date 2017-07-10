a733203b066af36a38aa82e505766d9d985a9df0
Enrico Icardi <enrico.icardi@gmail.com>
2017-07-10 20:22:41 +0200

    1st commit of basic setup

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+// JS from vendors from node_modules (installed with NPM)
+if (!global._babelPolyfill) {
+  require("babel-polyfill") //workaround to make this work with the iFrame refresh of CraftCMS preview
+}
+
+import $ from 'jquery';
+import objectFitImages from 'object-fit-images'; //polyfill for CSS object-fit
+//import whateverOtherJqueryPlugin from 'whateverJqueryPlugin';
+
+console.info( "\n\n\nCreated by %chttp://welance.com/ %ca Freelancers Collective.\n\n\n\n", "color: #FDB68E", "color: auto" );
+
+$(function() {
+    //console.info("Good News: Main.js file has been imported");
+});
